11d0b8eec44662ba47e3694071b797202d5b27ce
rstix <rsonroad@gmail.com>
2020-02-19 18:23:41 +0100

    modal component created

---
+++ client/src/components/ItemModal.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Form,
+  FormGroup,
+  Label,
+  Input
+} from 'reactstrap'
+import { connect } from 'react-redux'
+
+class ItemModal extends React.Component{
+
+}
+
+export default connect()(ItemModal)
+
+ 
