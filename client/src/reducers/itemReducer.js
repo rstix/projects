c256519833987d2db097f4964229e4344f7d24c6
rstix <rsonroad@gmail.com>
2020-02-19 16:32:31 +0100

    item reducers created

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,19 +1,24 @@
 import uuid from 'uuid'
-import { GET_ITEM, DELETE_ITEM, ADD_ITEM } from '../actions/types' 
+import { GET_ITEMS, DELETE_ITEM, ADD_ITEM } from '../actions/types' 
 
 const initialState = {
   items: [
     {id: uuid(),name: 'Egg'},
     {id: uuid(),name: 'Cheese'},
     {id: uuid(),name: 'Butter'},
-    {id: uuid(),name: 'Milk'}
+    {id: uuid(),name: 'Candy'}
   ]
 }
 
-export default function(state=initialState, action){
+export default function(state = initialState,  action ){
   switch(action.type){
-    case GET_ITEM:
+    case GET_ITEMS:
      return {...state}
+    case DELETE_ITEM:
+      return {
+        ...state,
+        items: state.items.filter(item=>item.id !== action.payload )
+      }
     default:
       return state
   } 
