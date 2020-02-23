49ea7f97994bb1473e6def3d1bc36468bb59cec0
rstix <rsonroad@gmail.com>
2020-02-23 21:41:03 +0100

    old button deleted

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -29,13 +29,6 @@ class ShoppingList extends Component{
         const {items} = this.props.item
         return (
             <Container>
-                <Button 
-                    color='dark'
-                    style={{marginBottom: '2rem'}}
-                    onClick={()=> this.addItem()}
-                    > 
-                    + Add Item 
-                </Button>
                 <ListGroup>
                     <TransitionGroup className="shopping-list">
                         {items.map(({id,name})=>(
