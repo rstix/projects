d3df152d77bfd3b3307b93f2bfd5f00ee2bd879e
rstix <rsonroad@gmail.com>
2020-02-23 21:40:32 +0100

    modal shown in the front end

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react';
 
 import AppNavbar from './components/AppNavbar'
 import ShoppingList from './components/ShoppingList'
+import ItemModal from './components/ItemModal'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
+import { Container } from 'reactstrap'
 import { Provider } from 'react-redux'
 import store from './store'
 
@@ -14,7 +17,10 @@ class App extends Component {
       <Provider store={store}>
         <div className="App">
           <AppNavbar/>
-          <ShoppingList/>
+          <Container>
+            <ItemModal/>
+            <ShoppingList/>
+          </Container>
         </div>
       </Provider>
     );
