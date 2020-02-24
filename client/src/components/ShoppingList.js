97a353b822fc195d3f94e9ec5e5240c5d44a0281
rstix <rsonroad@gmail.com>
2020-02-24 02:50:32 +0100

    uuid removed

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import uuid from 'uuid';
 import { connect } from 'react-redux';
 import { getItems, deleteItem } from '../actions/itemActions';
 import PropTypes from 'prop-types';
@@ -15,7 +14,7 @@ class ShoppingList extends Component {
     const name = prompt('Enter Item');
     if (name) {
       this.setState(state => ({
-        items: [...state.items, { id: uuid(), name }]
+        items: [...state.items, { name }]
       }));
     }
   };
